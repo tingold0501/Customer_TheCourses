c29051d339000bf2ab849ade85ba1c7a21aad092
Phạm Huỳnh Tín <65479150+tingold0501@users.noreply.github.com>
2024-01-10 12:59:02 +0700

    .....

--- src/pages/Course.jsx
+++ src/pages/Course.jsx
@@ -2,10 +2,18 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Courses from '../components/Courses';
 function Course() {
   return (
     <div>
         <Header/>
+        <div className="grid grid-cols-4 gap-4">
+        <Courses/>
+        <Courses/>
+        <Courses/>
+        <Courses/>
+        <Courses/>
+        </div>
         <Footer/>
     </div>
   )
