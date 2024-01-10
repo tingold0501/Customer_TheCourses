d439ae118dbcbe528e82375574fc22ef1ccd0842
Phạm Huỳnh Tín <65479150+tingold0501@users.noreply.github.com>
2024-01-10 16:45:09 +0700

    ....

---
+++ src/components/CategoryItem.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function CategoryItem({ props }) {
+  return (
+    <div>
+      <div className="flex flex-col items-center gap-3 px-8 py-10 bg-white drop-shadow-2xl rounded-3xl shadow-main">
+        <span>
+          {props.svg}
+        </span>
+        <p className="text-2xl font-extrabold text-dark-grey-900">{props.title}</p>
+        <p className="text-base text-center leading-7 text-dark-grey-600">
+          {props.detail}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default CategoryItem;
--- src/pages/Course.jsx
+++ src/pages/Course.jsx
@@ -9,13 +9,24 @@ function Course() {
     <div>
       <Header />
       <Category/>
-      <div className="grid grid-cols-4 gap-4">
+      <div className="grid grid-cols-4 gap-4 ml-[5%]">
         <Courses />
         <Courses />
         <Courses />
         <Courses />
-        <Courses />
+       
       </div>
+      <div className="flex flex-col items-center mt-12 text-center">
+      <span className="relative inline-flex w-full md:w-auto">
+        <a
+          href="#_"
+          className="inline-flex items-center justify-center w-full px-8 py-4 text-base font-bold leading-6 text-white bg-indigo-600 border border-transparent rounded-full md:w-auto hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"
+        >
+          Show All
+        </a>
+      
+      </span>
+    </div>
       <Footer />
     </div>
   )
