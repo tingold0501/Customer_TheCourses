f170c79a9e179dd5d965b92911652e895d49e01e
tingold0501 <huynhtin0501@gmail.com>
2024-07-29 08:42:42 +0700

    .....

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <GoogleOAuthProvider clientId='754368669319-tjdcd3ekmab7jk90p5lgaqferk9l44kj.apps.googleusercontent.com'>
+  <GoogleOAuthProvider clientId='744901264071-3geolvr1o45dol70hclm6ru8ht8vudp0.apps.googleusercontent.com'>
     <React.StrictMode>
       <App />
     </React.StrictMode>,
